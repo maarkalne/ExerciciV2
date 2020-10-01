e4f89f56c60157711200f277fc9ab19e39b05cb2
maarkalne <marc.alba01@gmail.com>
2020-10-01 18:40:49 +0200

    Initial commit

---
+++ src/Ex2v2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex2v2 {
+    public static void main(String[] args) {
+        Scanner sc_daw = new Scanner(System.in);
+
+        int primerNum;
+        int segonNum;
+        String nom;
+
+        System.out.println("Escriu el teu non");
+        nom = sc_daw.next();
+
+        System.out.println("Escriu el teu saldo");
+        segonNum = sc_daw.nextInt();
+
+
+
+
+    }
+}
