ea5584751bc1c08e5ae255440a4e6a0b5a8f2fc8
maarkalne <marc.alba01@gmail.com>
2020-10-01 19:56:47 +0200

    Exerici V3

---
+++ src/Exerici3v.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exerici3v {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+
+        System.out.println("Escriu la paraula Covid19");
+        long tempsInicial = System.currentTimeMillis();
+        String paraula= sc.nextLine();
+        if (paraula.equals("Covid19")){
+        long tempsFinal = (System.currentTimeMillis()-tempsInicial)/1000;
+            System.out.println(tempsFinal);
+            if (tempsFinal < 2) {
+                System.out.println("Ets un crack!!");
+            }
+            else if (tempsFinal < 3){
+                System.out.println("Ets força rapid");
+            }
+            else if (tempsFinal>3){
+                System.out.println("Ets molt lent");
+            }
+        }else{
+            System.out.println("La Paraula malescrita");
+
+        }
+
+
+
+
+
+
+    }
+}
