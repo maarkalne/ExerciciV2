152f2819d12eea473d7eb06de2f93f45acba6d2d
maarkalne <marc.alba01@gmail.com>
2020-10-01 19:32:02 +0200

    Exerici V2

--- src/Ex2v2.java
+++ src/Ex2v2.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 
 public class Ex2v2 {
     public static void main(String[] args) {
-        Scanner sc_daw = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
 
-        int primerNum;
+
         int segonNum;
         String nom;
 
         System.out.println("Escriu el teu non");
-        nom = sc_daw.next();
+        nom = sc.nextLine();
 
         System.out.println("Escriu el teu saldo");
-        segonNum = sc_daw.nextInt();
+        segonNum = sc.nextInt();
 
-
+        if (segonNum<=0) {
+            System.out.println(nom+" Estas amb numeros Negatius");
+        }else {
+            System.out.println(nom+ " Estas amb numeros Positius");
+        }
 
 
     }
